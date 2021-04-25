8d3fa7b084eb2e430871b9f1f6737c057c65224d
Akiiiira <nan10castle@fuji.waseda.jp>
2021-04-26 03:31:22 +0900

    modify code to read easily and add original code

--- thesne/examples/gaussians.py
+++ thesne/examples/gaussians.py
@@ -2,14 +2,13 @@ import numpy as np
 
 from sklearn.utils import check_random_state
 
-from thesne.model.dynamic_tsne import dynamic_tsne
-from thesne.examples import plot
+from thesne.thesne.model.dynamic_tsne import dynamic_tsne
+from thesne.thesne.examples import plot
 
 
-def create_blobs(classes=10, dims=100, class_size=100, variance=0.1, steps=4,
-                 advection_ratio=0.5, random_state=None):
+def create_blobs(classes=10, dims=10, class_size=100, variance=0.1, steps=4, advection_ratio=0.5, random_state=None):
     random_state = check_random_state(random_state)
-    X = []
+    original_data = []
 
     indices = random_state.permutation(dims)[0:classes]
     means = []
@@ -18,34 +17,45 @@ def create_blobs(classes=10, dims=100, class_size=100, variance=0.1, steps=4,
         mean[indices[c]] = 1.0
         means.append(mean)
 
-        X.append(random_state.multivariate_normal(mean, np.eye(dims)*variance,
-                                                  class_size))
-    X = np.concatenate(X)
-    y = np.concatenate([[i]*class_size for i in range(classes)])
+        original_data.append(random_state.multivariate_normal(mean, np.eye(dims) * variance, class_size))
+    original_data = np.concatenate(original_data)
+    class_label_list = np.concatenate([[i] * class_size for i in range(classes)])
 
-    Xs = [np.array(X)]
+    all_step_original_data = [np.array(original_data)]
     for step in range(steps - 1):
-        Xnext = np.array(Xs[step])
+        next_step_original_data = np.array(all_step_original_data[step])
         for c in range(classes):
-            stard, end = class_size*c, class_size*(c + 1)
-            Xnext[stard: end] += advection_ratio*(means[c] - Xnext[stard: end])
+            start, end = class_size * c, class_size * (c + 1)
+            next_step_original_data[start: end] += advection_ratio * (means[c] - next_step_original_data[start: end])
 
-        Xs.append(Xnext)
+        all_step_original_data.append(next_step_original_data)
 
-    return Xs, y
+    return all_step_original_data, class_label_list
 
 
 def main():
     seed = 0
+    steps = 10
+    all_step_original_data, class_label_list = create_blobs(classes=10, class_size=200, dims=100, advection_ratio=0.1,
+                                                            steps=steps, random_state=seed)
 
-    Xs, y = create_blobs(class_size=200, advection_ratio=0.1, steps=10,
-                         random_state=seed)
+    all_step_visible_data = dynamic_tsne(all_step_original_data, perplexity=70, penalty_lambda=0.1, verbose=0,
+                                         sigma_iters=50, random_state=seed)
 
-    Ys = dynamic_tsne(Xs, perplexity=70, lmbda=0.1, verbose=1, sigma_iters=50,
-                      random_state=seed)
+    for visible_data in all_step_visible_data:
+        plot.plot(visible_data, class_label_list)
 
-    for Y in Ys:
-        plot.plot(Y, y)
+    N = all_step_original_data[0].shape[0]
+    output_dims = all_step_visible_data[0].shape[1]
+    # add step axis
+    Ys_for_df = []
+    for s in range(steps):
+        step = np.array([s] * N)
+        Ys_for_df.append(np.concatenate((all_step_visible_data[s], np.expand_dims(step, axis=1)), axis=1))
+
+    Ys_for_df = np.array(Ys_for_df).reshape(-1, output_dims + 1)
+    columns = [f'dim{i}' for i in range(1, output_dims + 1)]
+    columns.append('step')
 
 
 if __name__ == "__main__":
--- thesne/model/dynamic_tsne.py
+++ thesne/model/dynamic_tsne.py
@@ -9,20 +9,20 @@ from .core import cost_var
 from .core import find_sigma
 
 
-def movement_penalty(Ys, N):
+def movement_penalty(all_step_visible_data, N):
     penalties = []
-    for t in range(len(Ys) - 1):
-        penalties.append(T.sum((Ys[t] - Ys[t + 1])**2))
+    for t in range(len(all_step_visible_data) - 1):
+        penalties.append(T.sum((all_step_visible_data[t] - all_step_visible_data[t + 1]) ** 2))
 
-    return T.sum(penalties)/(2*N)
+    return T.sum(penalties) / (2 * N)
 
 
-def find_Ys(Xs_shared, Ys_shared, sigmas_shared, N, steps, output_dims,
-            n_epochs, initial_lr, final_lr, lr_switch, init_stdev,
-            initial_momentum, final_momentum, momentum_switch, lmbda, metric,
-            verbose=0):
-    """Optimize cost wrt Ys[t], simultaneously for all t"""
-    # Optimization hyperparameters
+def find_all_step_visible_data(all_step_original_data_shared, all_step_visible_data_shared, sigmas_shared, N, steps,
+                               output_dims, n_epochs, initial_lr, final_lr, lr_switch, initial_momentum,
+                               final_momentum, momentum_switch, penalty_lambda, metric, verbose=0):
+    """Optimize cost wrt all_step_visible_data[t], simultaneously for all t"""
+
+    # Optimization hyper-parameters
     initial_lr = np.array(initial_lr, dtype=floath)
     final_lr = np.array(final_lr, dtype=floath)
     initial_momentum = np.array(initial_momentum, dtype=floath)
@@ -34,53 +34,52 @@ def find_Ys(Xs_shared, Ys_shared, sigmas_shared, N, steps, output_dims,
     momentum = T.fscalar('momentum')
     momentum_shared = theano.shared(initial_momentum)
 
-    # Penalty hyperparameter
-    lmbda_var = T.fscalar('lmbda')
-    lmbda_shared = theano.shared(np.array(lmbda, dtype=floath))
+    # Penalty hyper-parameter
+    penalty_lambda_var = T.fscalar('penalty_lambda')
+    penalty_lambda_shared = theano.shared(np.array(penalty_lambda, dtype=floath))
 
     # Yv velocities
-    Yvs_shared = []
+    all_step_visible_progress_shared = []
     zero_velocities = np.zeros((N, output_dims), dtype=floath)
     for t in range(steps):
-        Yvs_shared.append(theano.shared(np.array(zero_velocities)))
+        all_step_visible_progress_shared.append(theano.shared(np.array(zero_velocities)))
 
     # Cost
-    Xvars = T.fmatrices(steps)
-    Yvars = T.fmatrices(steps)
-    Yv_vars = T.fmatrices(steps)
+    all_step_original_data_vars = T.fmatrices(steps)
+    all_step_visible_data_vars = T.fmatrices(steps)
+    all_step_visible_progress_vars = T.fmatrices(steps)
     sigmas_vars = T.fvectors(steps)
 
     c_vars = []
     for t in range(steps):
-        c_vars.append(cost_var(Xvars[t], Yvars[t], sigmas_vars[t], metric))
+        c_vars.append(cost_var(all_step_original_data_vars[t], all_step_visible_data_vars[t], sigmas_vars[t], metric))
 
-    cost = T.sum(c_vars) + lmbda_var*movement_penalty(Yvars, N)
+    cost = T.sum(c_vars) + penalty_lambda_var * movement_penalty(all_step_visible_data_vars, N)
 
-    # Setting update for Ys velocities
-    grad_Y = T.grad(cost, Yvars)
+    # Setting update for all_step_visible_data velocities
+    grad_Y = T.grad(cost, all_step_visible_data_vars)
 
-    givens = {lr: lr_shared, momentum: momentum_shared,
-              lmbda_var: lmbda_shared}
+    givens = {lr: lr_shared, momentum: momentum_shared, penalty_lambda_var: penalty_lambda_shared}
     updates = []
     for t in range(steps):
-        updates.append((Yvs_shared[t], momentum*Yv_vars[t] - lr*grad_Y[t]))
+        updates.append((all_step_visible_progress_shared[t], momentum * all_step_visible_progress_vars[t] - lr * grad_Y[t]))
 
-        givens[Xvars[t]] = Xs_shared[t]
-        givens[Yvars[t]] = Ys_shared[t]
-        givens[Yv_vars[t]] = Yvs_shared[t]
+        givens[all_step_original_data_vars[t]] = all_step_original_data_shared[t]
+        givens[all_step_visible_data_vars[t]] = all_step_visible_data_shared[t]
+        givens[all_step_visible_progress_vars[t]] = all_step_visible_progress_shared[t]
         givens[sigmas_vars[t]] = sigmas_shared[t]
 
     update_Yvs = theano.function([], cost, givens=givens, updates=updates)
 
-    # Setting update for Ys positions
+    # Setting update for all_step_visible_data positions
     updates = []
     givens = dict()
     for t in range(steps):
-        updates.append((Ys_shared[t], Yvars[t] + Yv_vars[t]))
-        givens[Yvars[t]] = Ys_shared[t]
-        givens[Yv_vars[t]] = Yvs_shared[t]
+        updates.append((all_step_visible_data_shared[t], all_step_visible_data_vars[t] + all_step_visible_progress_vars[t]))
+        givens[all_step_visible_data_vars[t]] = all_step_visible_data_shared[t]
+        givens[all_step_visible_progress_vars[t]] = all_step_visible_progress_shared[t]
 
-    update_Ys = theano.function([], [], givens=givens, updates=updates)
+    update_all_step_visible_data = theano.function([], [], givens=givens, updates=updates)
 
     # Momentum-based gradient descent
     for epoch in range(n_epochs):
@@ -90,38 +89,38 @@ def find_Ys(Xs_shared, Ys_shared, sigmas_shared, N, steps, output_dims,
             momentum_shared.set_value(final_momentum)
 
         c = update_Yvs()
-        update_Ys()
+        update_all_step_visible_data()
         if verbose:
             print('Epoch: {0}. Cost: {1:.6f}.'.format(epoch + 1, float(c)))
 
-    Ys = []
+    all_step_visible_data = []
     for t in range(steps):
-        Ys.append(np.array(Ys_shared[t].get_value(), dtype=floath))
+        all_step_visible_data.append(np.array(all_step_visible_data_shared[t].get_value(), dtype=floath))
 
-    return Ys
+    return all_step_visible_data
 
 
-def dynamic_tsne(Xs, perplexity=70, Ys=None, output_dims=2, n_epochs=1000,
+def dynamic_tsne(all_step_original_data, perplexity=30, all_step_visible_data=None, output_dims=2, n_epochs=1000,
                  initial_lr=2400, final_lr=200, lr_switch=250, init_stdev=1e-4,
                  sigma_iters=50, initial_momentum=0.5, final_momentum=0.8,
-                 momentum_switch=250, lmbda=0.1, metric='euclidean',
+                 momentum_switch=250, penalty_lambda=0.1, metric='euclidean',
                  random_state=None, verbose=1):
     """Compute sequence of projections from a sequence of matrices of
     observations (or distances) using dynamic t-SNE.
     
     Parameters
     ----------
-    Xs : list of array-likes, each with shape (n_observations, n_features), \
+    all_step_original_data : list of array-likes, each with shape (n_observations, n_features), \
             or (n_observations, n_observations) if `metric` == 'precomputed'.
         List of matrices containing the observations (one per row). If `metric` 
         is 'precomputed', list of pairwise dissimilarity (distance) matrices. 
-        Each row in `Xs[t + 1]` should correspond to the same row in `Xs[t]`, 
+        Each row in `all_step_original_data[t + 1]` should correspond to the same row in `all_step_original_data[t]`, 
         for every time step t > 1.
     
     perplexity : float, optional (default = 30)
         Target perplexity for binary search for sigmas.
         
-    Ys : list of array-likes, each with shape (n_observations, output_dims), \
+    all_step_visible_data : list of array-likes, each with shape (n_observations, output_dims), \
             optional (default = None)
         List of matrices containing the starting positions for each point at
         each time step.
@@ -158,12 +157,12 @@ def dynamic_tsne(Xs, perplexity=70, Ys=None, output_dims=2, n_epochs=1000,
     momentum_switch : int, optional (default = 250)
         Iteration in which the momentum changes from initial to final.
         
-    lmbda : float, optional (default = 0.0)
+    penalty_lambda : float, optional (default = 0.0)
         Movement penalty hyperparameter. Controls how much each point is
         penalized for moving across time steps.
         
     metric : 'euclidean' or 'precomputed', optional (default = 'euclidean')
-        Indicates whether `X[t]` is composed of observations ('euclidean') 
+        Indicates whether `original_data[t]` is composed of observations ('euclidean') 
         or distances ('precomputed'), for all t.
     
     random_state : int or np.RandomState, optional (default = None)
@@ -176,51 +175,41 @@ def dynamic_tsne(Xs, perplexity=70, Ys=None, output_dims=2, n_epochs=1000,
         
     Returns
     -------
-    Ys : list of array-likes, each with shape (n_observations, output_dims)
+    all_step_visible_data : list of array-likes, each with shape (n_observations, output_dims)
         List of matrices representing the sequence of projections. 
-        Each row (point) in `Ys[t]` corresponds to a row (observation or 
-        distance to other observations) in the input matrix `Xs[t]`, for all t.
+        Each row (point) in `all_step_visible_data[t]` corresponds to a row (observation or 
+        distance to other observations) in the input matrix `all_step_original_data[t]`, for all t.
     """
     random_state = check_random_state(random_state)
 
-    steps = len(Xs)
-    N = Xs[0].shape[0]
+    steps = len(all_step_original_data)
+    N = all_step_original_data[0].shape[0]
 
-    if Ys is None:
-        Y = random_state.normal(0, init_stdev, size=(N, output_dims))
-        Ys = [Y]*steps
+    if all_step_visible_data is None:
+        initial_visible_data = random_state.normal(0, init_stdev, size=(N, output_dims))
+        all_step_visible_data = [initial_visible_data] * steps
 
     for t in range(steps):
-        if Xs[t].shape[0] != N or Ys[t].shape[0] != N:
+        if all_step_original_data[t].shape[0] != N or all_step_visible_data[t].shape[0] != N:
             raise Exception('Invalid datasets.')
 
-        Xs[t] = np.array(Xs[t], dtype=floath)
+        all_step_original_data[t] = np.array(all_step_original_data[t], dtype=floath)
 
-    Xs_shared, Ys_shared, sigmas_shared = [], [], []
+    all_step_original_data_shared, all_step_visible_data_shared, sigmas_shared = [], [], []
     for t in range(steps):
-        X_shared = theano.shared(Xs[t])
+        original_data_shared = theano.shared(all_step_original_data[t])
         sigma_shared = theano.shared(np.ones(N, dtype=floath))
 
-        find_sigma(X_shared, sigma_shared, N, perplexity, sigma_iters,
+        find_sigma(original_data_shared, sigma_shared, N, perplexity, sigma_iters,
                    metric=metric, verbose=verbose)
 
-        Xs_shared.append(X_shared)
-        Ys_shared.append(theano.shared(np.array(Ys[t], dtype=floath)))
+        all_step_original_data_shared.append(original_data_shared)
+        all_step_visible_data_shared.append(theano.shared(np.array(all_step_visible_data[t], dtype=floath)))
         sigmas_shared.append(sigma_shared)
 
-    Ys = find_Ys(Xs_shared, Ys_shared, sigmas_shared, N, steps, output_dims,
-                 n_epochs, initial_lr, final_lr, lr_switch, init_stdev,
-                 initial_momentum, final_momentum, momentum_switch, lmbda,
-                 metric, verbose)
-    
-    # add step axis
-    Ys_for_df = []
-    for s in range(steps):
-        step = np.array([s] * N)
-        Ys_for_df.append(np.concatenate((Ys[s], np.expand_dims(step, axis=1)), axis=1))
-    Ys_for_df = np.array(Ys_for_df).reshape(-1, output_dims+1)
+    all_step_visible_data = find_all_step_visible_data(all_step_original_data_shared, all_step_visible_data_shared,
+                                                       sigmas_shared, N, steps, output_dims,
+                                                       n_epochs, initial_lr, final_lr, lr_switch, initial_momentum,
+                                                       final_momentum, momentum_switch, penalty_lambda, metric, verbose)
 
-    columns = [f'dim{i}' for i in range(1, output_dims + 1)]
-    columns.append('step')
-
-    return Ys, Ys_for_df, columns
+    return all_step_visible_data
